1f6837506adda3a7c5d5e586ec878898fc0faacd
Nadia Makarevich <nmakarevich@atlassian.com>
2019-09-13 20:21:34 +1000

    typescript

---
+++ src/index.js
@@ -0,0 +1,5 @@
+var test = function (_a) {
+    var a = _a.a, b = _a.b;
+    return a + b;
+};
+var another = test({ a: 1, b: 2 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export default () => {
-    return "test"
+interface Test {
+    a: number,
+    b: number
 }
+
+const test = ({ a, b }: Test) => {
+    return a + b;
+}
+
+const another = test({ a: 1, b: 2 });
